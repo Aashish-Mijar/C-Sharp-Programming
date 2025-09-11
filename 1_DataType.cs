820cf02eeea00a04974a978cc2de157c35301f8c
rep-demo <aashishmijar44@gmail.com>
2025-09-11 21:56:45 +0545

    DataType in C #

---
+++ 1_DataType.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basics
+{
+    internal class _1_DataType
+    {
+        internal void DataType()
+        {
+            // Instead of using datatype for each we can use 'var'
+            byte number = 9;  // var number
+            int count = 10; // var count
+            float totalPrice = 20.94f;   // var totalPrice
+            char character = 'A';    // var character
+            string firstName = "Aashish"; // var string
+            bool isWorking = true;  // bool
+
+            Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(totalPrice);
+            Console.WriteLine(character);
+            Console.WriteLine(firstName);
+            Console.WriteLine(isWorking);
+
+            Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
+        }
+    }
+}
