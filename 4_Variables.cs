7c4cdc0ab6877f17b7db82f6ae6ef34c68da5491
rep-demo <aashishmijar44@gmail.com>
2025-09-11 22:04:47 +0545

    Varibles in C#

---
+++ 4_Variables.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basics
+{
+    internal class _4_Variables
+    {
+        // Global Variable  Decalared under calss
+       static int y = 100;
+
+    internal void variables()
+        {
+              // Local variable
+            int y = 4;
+            Console.WriteLine("The value of y is " + y);
+            y = 6;
+            Console.WriteLine("The value of y is " + y);
+            Console.WriteLine("The value of y is " + _4_Variables.y);
+
+        }
+    }
+}
