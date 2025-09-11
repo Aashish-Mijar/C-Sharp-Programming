a8ba812f1c5724bc396919f81feae8bda197e67e
rep-demo <aashishmijar44@gmail.com>
2025-09-11 22:03:29 +0545

    Type conversion in C#

---
+++ 2_TypeConversion.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basics
+{
+    class _2_TypeConversion
+    {
+        internal void Run()
+        {
+
+            int i = 3;
+            //byte b = i;  // won't compile
+
+            //Explict type conversion
+            byte b = (byte)i;
+
+            float f = 1.0f;
+            int fl = (int)f;
+
+
+            Console.WriteLine(b);
+            Console.WriteLine(fl);
+
+
+            // Non-compatible types
+            //string s = "a";
+            //int str = (int)s;
+
+            string str = "1";
+            int x = Convert.ToInt32(str);
+            int j = int.Parse(str);
+
+            Console.WriteLine(j);
+        }
+    }
+}
+
