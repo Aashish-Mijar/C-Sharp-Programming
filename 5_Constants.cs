635c08e97918b164022f2bde02eadd268c28ac33
rep-demo <aashishmijar44@gmail.com>
2025-09-11 22:06:23 +0545

    Constants in C#

---
+++ 5_Constants.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basics
+{
+    internal class _5_Constants
+    {
+        internal void MyConstant()
+        {
+            const String subject = "C Sharp";
+            Console.WriteLine(subject);
+
+            // Cannot be performed
+            //subject = "C programming"; 
+
+
+            //-----Literals--------
+            // It is the value of variable and constant
+            // String literals: "Hello", "Hi"..
+            // Float literals: 12.34.... and many other types
+        }
+    }
+}
