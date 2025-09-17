38fc9f5a20b9e4b4696714ea224f2bd28817df49
rep-demo <aashishmijar44@gmail.com>
2025-09-17 17:24:44 +0545

    Logical operators

---
+++ 4_Logical.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators
+{
+    internal class _4_Logical
+    {
+        static void Main()
+        {
+            int x = 23;
+            // logical and
+            Console.WriteLine(x > 3 && x < 10);
+
+            // logical or
+            Console.WriteLine(x < 5 || x < 3);
+
+            // Logical not
+            Console.WriteLine(!(x> 3 && x < 10));
+        }
+    }
+}
