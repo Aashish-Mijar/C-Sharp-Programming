685a2848d829e6135d9612179419790e0e9d90a8
rep-demo <aashishmijar44@gmail.com>
2025-10-08 15:30:48 +0545

    Problem 8 Solution

---
+++ Assignment_One/08_Solution.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_One
+{
+    internal class _08_Solution
+    {
+        internal void vowel_and_const_counter()
+        {
+            string str = "programming";
+            int countVowel = 0;
+            int countConsonant = 0;
+            foreach (char c in str)
+            {
+                //Console.WriteLine(c);
+                if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
+                {
+                    countVowel++;   
+                }
+                else
+                {
+                    countConsonant++;
+                }
+            }
+
+            Console.WriteLine("Vowel count: " + countVowel);
+            Console.WriteLine("Consonant count: " + countConsonant);
+         }   
+    }
+}
