07773c1a3232c7e4043dde2266171043258dd208
rep-demo <aashishmijar44@gmail.com>
2025-10-08 15:13:30 +0545

    Problem 6 Solution

---
+++ Assignment_One/06_Solution.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_One
+{
+    internal class _06_Solution
+    {
+        internal void multiplicationTable()
+        {
+            int n = 5;
+            int i;
+            for(i = 10; i > 0; i--)
+            {
+                int multiply = n * i; 
+                Console.WriteLine("{0} x {1} = {2}", n, i, multiply);
+            }
+        }
+    }
+}
