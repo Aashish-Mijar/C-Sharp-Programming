c68f7873b77d80c80126dd9f12c743f9c5f168df
rep-demo <aashishmijar44@gmail.com>
2025-10-08 15:33:20 +0545

    Boxing and Unboxing in C #

---
+++ 6_BoxingUnBoxing.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basics
+{
+    internal class _6_BoxingUnBoxing
+    {
+        // Boxing  ---- int to Object
+        internal void BoxingFun()
+        {
+            int a = 10;
+            Object obj;
+            obj = a;
+            Console.WriteLine(obj);
+        }
+
+        // Unxboxing ----- object to int
+        internal void UnBoxingFun()
+        {
+            Object obj = 10;
+            int a = (int)obj;
+            Console.WriteLine(a);
+        }
+    }
+}
