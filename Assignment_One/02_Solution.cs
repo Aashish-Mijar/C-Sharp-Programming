eaacebfb2f4e476a9cc9e23eafba6a82eb068fa7
rep-demo <aashishmijar44@gmail.com>
2025-10-08 15:08:33 +0545

    Problem 2 Solution

---
+++ Assignment_One/02_Solution.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_One
+{
+    internal class _02_Solution
+    {
+        internal void LeapYearCheck()
+        {
+            Console.WriteLine("Enter a year:");
+            int year = Convert.ToInt32(Console.ReadLine());
+
+            if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)) {
+                Console.WriteLine(year + " is a leap year");
+            }
+            else
+            {
+                Console.WriteLine(year + " is not a leap year");
+            }
+        } 
+    }
+}
