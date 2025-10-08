027375647cb61d592866ed8a41aff0d94505d830
rep-demo <aashishmijar44@gmail.com>
2025-10-08 15:04:05 +0545

    Problem 1 Solution

---
+++ Assignment_One/01_Solution.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_One
+{
+    internal class _01_Solution
+    {
+       internal void swapNbr() {
+
+            int a = 8, b = 3;
+            Console.WriteLine("The value of a: {0} and b: {1} before swapping.", a, b);
+
+            // Swapping logic without third variable
+            a = a + b;
+            b = a - b;
+            a = a - b;
+
+            // Next method
+            //a = a ^ b;
+            //b = a ^ b;
+            //a = a ^ b;
+
+            Console.WriteLine("The value of a: {0} and b: {1} after swapping.", a, b);
+        }
+    }
+}
