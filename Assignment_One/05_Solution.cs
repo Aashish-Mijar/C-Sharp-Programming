69d02442a6fbb858042d7562b06f2f85a858e06d
rep-demo <aashishmijar44@gmail.com>
2025-10-08 15:12:10 +0545

    Problem 5 Solution

---
+++ Assignment_One/05_Solution.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_One
+{
+    internal class _05_Solution
+    {
+        internal void Factorial()
+        {
+            int n = 6;
+            int factNum = 1;
+            while (n > 0)
+            {
+                factNum *= n;
+                n--;
+            }
+            Console.WriteLine("The factorial of 6 is {0}", factNum);
+        }
+    }
+}
