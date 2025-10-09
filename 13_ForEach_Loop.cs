4f986fd01321b511edd9ac0c8e206b54dbda318e
rep-demo <aashishmijar44@gmail.com>
2025-10-09 21:40:41 +0545

    For Each loop

---
+++ 13_ForEach_Loop.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basics
+{
+    internal class _13_ForEach_Loop
+    {
+        static void Main()
+        {
+            string[] cars = { "Volvo", "BMW", "Ford", "Mazda" };
+            foreach(string i in cars)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
