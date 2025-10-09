5ded31b3ff84f2e250c1052cd0e818e86ffe6fa3
rep-demo <aashishmijar44@gmail.com>
2025-10-09 21:39:31 +0545

    Loops in CSharp

---
+++ 11_Loops.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basics
+{
+    internal class _10_Loops
+    {
+        static void Main()
+        {
+            // ------ Loop -> can execute a block of code as long as a specified condition is reached.
+            int i = 0;
+            while (i < 5)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+             // ----- Do While Loop -----
+            do
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+            while (i < 5);
+
+            // ---- For Loop -----
+            for (i = 0; i < 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
