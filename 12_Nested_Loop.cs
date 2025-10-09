33e5ad0f63169382fb3146da2fb8a82b1f762c0c
rep-demo <aashishmijar44@gmail.com>
2025-10-09 21:40:07 +0545

    Nested Loop in CSharp

---
+++ 12_Nested_Loop.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basics
+{
+    internal class _12_Nested_Loop
+    {
+        static void Main()
+        {
+            // Outer Loop
+            for (int i = 1; i <= 2; ++i)
+            {
+
+                Console.WriteLine("Outer: " + i);
+
+                // Inner Loop
+                for (int j = 1; j <= 3; j++)
+                {
+                    Console.WriteLine("Inner: " + j);
+                }
+            }
+
+        }
+    }
+}
