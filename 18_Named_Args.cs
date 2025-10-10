590b492346e46bc095b1f0d286a4bb39fe947304
rep-demo <aashishmijar44@gmail.com>
2025-10-10 21:18:44 +0545

    Named Args in csharp

---
+++ 18_Named_Args.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basics
+{
+    internal class _18_Named_Args
+    {
+        // It is possible to send arguments with key: value syntax.
+        // The order of the arguments does not matter.
+        static void myMethod(string child1, string child2, string child3)
+        {
+            Console.WriteLine("The youngest child is: " + child3);
+        }
+       static void Main()
+        {
+            myMethod(child3: "John", child1: "liam", child2: "Luis");
+
+        }
+    }
+}
