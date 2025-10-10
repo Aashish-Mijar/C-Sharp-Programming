2b9a81ae16c6d97d2d24c93f7cb915b05a058bdc
rep-demo <aashishmijar44@gmail.com>
2025-10-10 21:19:17 +0545

    Method overloading

---
+++ 19_Method_Overloading.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basics
+{
+    internal class _19_Method_Overloading
+    {
+        static int add(int x, int y)
+        {
+            return x + y;
+        }
+
+        static double add(double x, double y)
+        {
+            return x + y;
+        }
+        static void Main()
+        {
+            int resultOne = add(4, 6);
+            double resultTwo = add(6.7, 2.4);
+            Console.WriteLine("Sum of integers: " + resultOne);
+            Console.WriteLine("Sum of double: " + resultTwo);
+        }
+    }
+}
