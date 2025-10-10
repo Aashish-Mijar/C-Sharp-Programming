aeb287ccf74bc0a916adfc07d5ea378caf0178ae
rep-demo <aashishmijar44@gmail.com>
2025-10-10 21:16:26 +0545

    Break and Continue in csharp

---
+++ 14_Break_Continue.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basics
+{
+    internal class _14_Break_Continue
+    {
+        static void Main()
+        {
+            // Break and Continue
+            // Break statement is used to jump out of a loop
+
+            for (int i = 0; i<10; i++)
+            {
+                if (i == 4)
+                {
+                    break;
+                }
+                Console.WriteLine(i);
+            }
+
+            // Continue statement breaks one iteration (in the loop), if a specified condition occurs, and continues with the next iteration in the loop
+
+            for(int i = 0; i<10; i++)
+            {
+                if(i == 4)
+                {
+                    continue;
+                }
+                Console.WriteLine(i);
+            }
+        }
+
+
+    }
+}
