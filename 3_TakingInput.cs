cedfc7083054c77b8c150e5bfa26f039076a29ee
rep-demo <aashishmijar44@gmail.com>
2025-10-10 21:22:33 +0545

    File modified

--- 3_TakingInput.cs
+++ 3_TakingInput.cs
@@ -9,9 +9,9 @@ namespace Basics
     internal class _3_TakingInput
     {
         internal void inputs() {
-            //Console.WriteLine("Enter username");
-            //string userName = Console.ReadLine();
-            //Console.WriteLine("Username is:" + userName);
+            // Console.WriteLine("Enter username");
+            // string userName = Console.ReadLine();
+            // Console.WriteLine("Username is:" + userName);
 
             int a, b, sum;
             Console.WriteLine("Enter number one: ");
@@ -21,8 +21,10 @@ namespace Basics
             b = Convert.ToInt32(Console.ReadLine());
 
             sum = a + b;
-
+            
+            Console.WriteLine("The value of a is {0} and b is {1}", a, b);
             Console.WriteLine("The sum of a and b is :" + sum);
+
 
         }
     }
