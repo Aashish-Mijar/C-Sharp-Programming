9452f2b5d1edd09d669579648c94cab24323601a
rep-demo <aashishmijar44@gmail.com>
2025-10-17 18:30:14 +0545

    Loops updated

--- 11_Loops.cs
+++ 11_Loops.cs
@@ -31,8 +31,9 @@ namespace Basics
                 Console.WriteLine(i);
             }
 
+            
             for (i = 0; i<5; i++){
-                for (int j = 0; j<5; j++){
+                for(int j = 0; j<5; j++){
                     Console.WriteLine("*");
                 }
             }
