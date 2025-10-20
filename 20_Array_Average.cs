9ee2a5b69c7b4fc5c769a8df68b4a6331e7fd76e
rep-demo <aashishmijar44@gmail.com>
2025-10-20 18:53:18 +0545

    Average of marks

---
+++ 20_Array_Average.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basics
+{
+    internal class _20_Array_Average
+    {
+        internal void ArrayAverage()
+        {
+         int[] marks = new int[5];
+         int total = 0;
+
+        }
+    }
+}
