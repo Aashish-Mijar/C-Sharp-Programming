4ac6422408e9af523e0821e293a7db470454ecb5
rep-demo <aashishmijar44@gmail.com>
2025-10-20 19:04:25 +0545

    loop logic for min and max

--- 21_Largest_in_Array.cs
+++ 21_Largest_in_Array.cs
@@ -20,6 +20,14 @@ namespace Basics
 
             int max = numbers[0];
             int min = numbers[0];
+
+            for(int i=1; i<numbers.length; i++){
+                if(numbers[i]>max)
+                    max = numbers[i]
+                if(numbers[i]<min)
+                    min = numbers[i]
+                
+            }
            
         }
     }
