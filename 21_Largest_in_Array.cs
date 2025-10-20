e3dca21e044139a891a01092fda87cc52d2dfe3a
rep-demo <aashishmijar44@gmail.com>
2025-10-20 19:00:30 +0545

    First commit

---
+++ 21_Largest_in_Array.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basics
+{
+    internal class _21_Largest_in_Array
+    {
+        internal void LargestInArray()
+        {
+            int[] numbers = new int[5];
+            Console.WriteLine("Enter 5 numbers: ");
+           
+        }
+    }
+}
