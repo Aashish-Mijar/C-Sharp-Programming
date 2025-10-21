529ac3eef7c6e5d47fb36705d1440de1d98099df
rep-demo <aashishmijar44@gmail.com>
2025-10-21 10:03:19 +0545

    Named arguments

---
+++ 23_NamedArgs2.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basics
+{
+    internal class _21_Largest_in_Array
+    {
+        internal void LargestInArray()
+        {
+            int[] numbers = new int[5];
+            Console.WriteLine("Enter 5 numbers: ");
+
+            for (int i = 0; i<5; i++){
+                Console.Write($"Number {i+1}: ");
+                numbers[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            int max = numbers[0];
+            int min = numbers[0];
+
+            for(int i=1; i<numbers.length; i++){
+                if(numbers[i]>max)
+                    max = numbers[i]
+                if(numbers[i]<min)
+                    min = numbers[i]
+                
+            }
+           
+           Console.WriteLine($"\nLargest number: {max}");
+           Console.WriteLine($"Smallest number: {min}");
+        }
+    }
+}
