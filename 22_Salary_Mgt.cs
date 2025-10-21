09259b324155471d03517fa81654a42b49b409c3
rep-demo <aashishmijar44@gmail.com>
2025-10-21 09:54:19 +0545

    First commit

--- 22_Salary_Mgt.cs
+++ 22_Salary_Mgt.cs
@@ -6,31 +6,13 @@ using System.Threading.Tasks;
 
 namespace Basics
 {
-    internal class _21_Largest_in_Array
-    {
-        internal void LargestInArray()
-        {
-            int[] numbers = new int[5];
-            Console.WriteLine("Enter 5 numbers: ");
-
-            for (int i = 0; i<5; i++){
-                Console.Write($"Number {i+1}: ");
-                numbers[i] = Convert.ToInt32(Console.ReadLine());
-            }
-
-            int max = numbers[0];
-            int min = numbers[0];
-
-            for(int i=1; i<numbers.length; i++){
-                if(numbers[i]>max)
-                    max = numbers[i]
-                if(numbers[i]<min)
-                    min = numbers[i]
-                
-            }
-           
-           Console.WriteLine($"\nLargest number: {max}");
-           Console.WriteLine($"Smallest number: {min}");
+    internal class _22_Salary_Mgt
+    { 
+        static void Main(){
+            string[] employees = {"Rita", "Suman", "Aashish", "Mira", "Kiran"}
+            
         }
+     
+
     }
 }
