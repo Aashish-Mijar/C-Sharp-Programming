9c5c4f993041cd07a24277110f969448aa7ac6f4
rep-demo <aashishmijar44@gmail.com>
2025-10-21 10:05:15 +0545

    function added

--- 23_NamedArgs2.cs
+++ 23_NamedArgs2.cs
@@ -6,31 +6,16 @@ using System.Threading.Tasks;
 
 namespace Basics
 {
-    internal class _21_Largest_in_Array
+    internal class _23_NamedArgs2
     {
-        internal void LargestInArray()
-        {
-            int[] numbers = new int[5];
-            Console.WriteLine("Enter 5 numbers: ");
-
-            for (int i = 0; i<5; i++){
-                Console.Write($"Number {i+1}: ");
-                numbers[i] = Convert.ToInt32(Console.ReadLine());
-            }
-
-            int max = numbers[0];
-            int min = numbers[0];
-
-            for(int i=1; i<numbers.length; i++){
-                if(numbers[i]>max)
-                    max = numbers[i]
-                if(numbers[i]<min)
-                    min = numbers[i]
-                
-            }
-           
-           Console.WriteLine($"\nLargest number: {max}");
-           Console.WriteLine($"Smallest number: {min}");
+        static void DisplayInfo(string name, int age){
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"Age: {age}");
         }
+        static void Main(){
+
+        }
+       
+            
     }
 }
