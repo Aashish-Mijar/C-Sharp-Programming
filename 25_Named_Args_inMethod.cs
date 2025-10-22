483957744b9b9c8359d6713df3790152e7966ca0
rep-demo <aashishmijar44@gmail.com>
2025-10-22 14:01:20 +0545

    Printing code

--- 25_Named_Args_inMethod.cs
+++ 25_Named_Args_inMethod.cs
@@ -9,7 +9,7 @@ namespace Basics
     internal class _25_Named_Args_inMethod
     {
         static void CreateStudent(string name, int age, string course, double gpa, string city){
-
+            Console.WriteLIne($"Name: {name}, Age:{age}, Course: {course}, GPA: {gpa}, City:{city}");
         }
       
             
