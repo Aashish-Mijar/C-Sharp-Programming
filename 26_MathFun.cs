8bbb5fc0813ca25c3a00398eb71fe7e2f666ed2d
rep-demo <aashishmijar44@gmail.com>
2025-10-23 16:46:28 +0545

    Static function

--- 26_MathFun.cs
+++ 26_MathFun.cs
@@ -6,24 +6,12 @@ using System.Threading.Tasks;
 
 namespace Basics
 {
-    internal class _25_Named_Args_inMethod
+    internal class _26_MathFun
     {
-        static void CreateStudent(string name, int age, string course, double gpa, string city){
-            Console.WriteLIne($"Name: {name}, Age:{age}, Course: {course}, GPA: {gpa}, City:{city}");
+        static void Main(){
+            
         }
-      
-        static void Main() {
-            // Without named arguments (hard to read)
-            CreateStudent("Mira", 20, "BCA", 3.8, "Waling");
+       
+        }
+  }
 
-            // With named arguments (clear!)
-            CreateStudent(
-                name: "Suman",
-                age: 21,
-                course:"BBA",
-                gpa:3.6,
-                city:"Pokhara"
-            );
-        }
-    }
-}
