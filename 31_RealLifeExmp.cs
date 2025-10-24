0aef199e77add7bd8b1b07d03631a556065f9518
rep-demo <aashishmijar44@gmail.com>
2025-10-24 18:35:45 +0545

    storing data in object array

--- 31_RealLifeExmp.cs
+++ 31_RealLifeExmp.cs
@@ -11,6 +11,10 @@ namespace Basics
        
        static void Main(){
         object[] data = new object[3];
+
+        data[0] = 10;
+        data[1] = 20.5;
+        data[2] = "C#";
        }
         }
   }
